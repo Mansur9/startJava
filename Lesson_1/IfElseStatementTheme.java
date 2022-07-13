23780f7d418e2c89f93c672cc4b9ee0a6ddd1482
Mansur Seyitmedov <>
2022-07-13 21:06:59 +0300

    fix bag

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -4,6 +4,7 @@ public class IfElseStatementTheme {
         // - перепишите псевдокод, приведенный ниже, с помощью Java
         // - используйте переменные, условные операторы, логическое НЕ
         // - придумайте сообщения и отобразите в консоли 
+        System.out.println("Перевод псевдокода на язык Java (это название задачи)");
         int age = 28;
         int height = 178;
         boolean maleGender = true;
@@ -39,6 +40,7 @@ public class IfElseStatementTheme {
         // - создайте две переменные, присвоив им любые числа
         // - найдите максимальное и минимальное число
         // - выведите числа на консоль, указав какое из них max и min
+        System.out.println("Поиск max и min числа");
         System.out.println("search the max and min numbers ");
         int number1 = 23;
         int number2 = 30;
@@ -58,6 +60,7 @@ public class IfElseStatementTheme {
                 // - отрицательным (положительным)
         // - если число равно 0, то остальные проверки не должны осуществляться
         // - отобразите полученные результаты
+        System.out.println("Работа с числом");
         int numberAny = -222;
         
         if (numberAny != 0) { 
@@ -68,7 +71,7 @@ public class IfElseStatementTheme {
             } if (numberAny > 0) {
                 System.out.println("положительное число");
             } else {
-                System.out.println("отрицателньое число");
+                System.out.println("отрицательное число");
             }
         } else {
             System.out.println("Равен нулю" + "\n3");
@@ -81,34 +84,34 @@ public class IfElseStatementTheme {
         //  - исходные числа
         //  - одинаковые в них цифры
         //  - номера разрядов
+        System.out.println("Поиск одинаковых цифр в числах");
         int srcNumber = 123;
-        int srcNumberSecond =321;
-        int hundreds = (srcNumber / 100) % 10; //сотни - получаем одну ед сотен
-        int dozens = (srcNumber / 10) % 10; //десятки - получаем десятки
-        int ones = srcNumber % 10; //единицы - получаем единицы
-        int hundredsSecond = (srcNumberSecond / 100) % 10; //сотни 
-        int dozensSecond = (srcNumberSecond / 10) % 10; //десятки 
-        int onesSecond = srcNumberSecond % 10; //единицы
-
-        System.out.println(hundreds + " первое число;"); 
-        System.out.println(dozens + " второе число");
-        System.out.println(ones + " третье число" + "\n");
-        System.out.println(hundredsSecond + " первое число;"); 
-        System.out.println(dozensSecond + " второе число");
-        System.out.println(onesSecond + " третье число");
+        int srcNumber2 =321;
+        int digitsHundreds = (srcNumber % 10; //сотни - получаем одну ед сотен
+        System.out.println(digitsHundreds + " первое число;"); 
+        int digitsDozens = (srcNumber / 10) % 10; //десятки - получаем десятки
+        System.out.println(digitsDozens + " второе число");
+        int digitsOnes = srcNumber % 10; //единицы - получаем единицы
+        System.out.println(digitsOnes + " третье число" + "\n");
+        int digitsHundredsSecond = (srcNumber2 / 100) % 10; //сотни 
+        System.out.println(digitsHundredsSecond + " первое число;"); 
+        int digitsDozensSecond = (srcNumber2 / 10) % 10; //десятки 
+        System.out.println(digitsDozensSecond + " второе число");
+        int digitsOnesSecond = srcNumber2 % 10; //единицы
+        System.out.println(digitsOnesSecond + " третье число");
         System.out.println();
 
-        if (hundreds != hundredsSecond) {
+        if (digitsHundreds != digitsHundredsSecond) {
             System.out.println("первые чсило не совпали");
         } else {
                 System.out.println("первые числа совпали " + hundreds);
         }
-        if (dozens != dozensSecond) {
+        if (digitsDozens != digitsDozensSecond) {
             System.out.println("вторые число не совпали");
         } else {
             System.out.println("вторые числа совпали " + dozens);
         }
-        if (ones != onesSecond) {
+        if (digitsOnes != digitsOnesSecond) {
             System.out.println("третьи числа не совпали");
         } else {
             System.out.println("третьи число совпали" + ones);
