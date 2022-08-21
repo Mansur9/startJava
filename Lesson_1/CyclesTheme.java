c26ad0306ac043d961779f71aa8cd3ffb45aa682
Mansur <maylur@yandex.ru>
2022-08-21 17:36:12 +0300

    fix bag

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -3,21 +3,20 @@ public class CyclesTheme {
         System.out.println("Подсчет суммы четных и нечетных чисел");
         int endRange = 21;
         int startRange = -10;
-        int oldNumberSum = 0;
-        int evenNumberCount = 0;
+        int evenSumNumber = 0;
+        int oddNumberCount = 0;
         do {
-            startRange = startRange + 1;
+            startRange++;
             if(startRange % 2 == 0) { 
-                evenNumberCount = evenNumberCount + startRange;
+                oddNumberCount = oddNumberCount + startRange;
             } else {
-                oldNumberSum = oldNumberSum + startRange;
+                evenSumNumber = evenSumNumber + startRange;
             }
         } while (endRange >= startRange);
-        System.out.println("Cумма четных = " +evenNumberCount);
-        System.out.println("Сумма нечетных = " + oldNumberSum);
-        System.out.println();
+        System.out.println("Сумма четных = " +evenSumNumber);
+        System.out.println("Сумма нечетных = " + oddNumberCount);
 
-        System.out.println("Вывод чисел в интервале (min и max) в порядке убывания");
+        System.out.println("\nВывод чисел в интервале (min и max) в порядке убывания");
         // Вывод чисел в интервале (min и max) в порядке убывания
         // даны значения 10, 5, -1
         // найдите среди них max и min число
@@ -27,29 +26,36 @@ public class CyclesTheme {
         int number1 = 10;
         int number2 = 5;
         int number3 = -1;
+        int max = 0;
+        int min = 0; 
         
-        if (number1 >= number2 || number1 >= number3 ) {
-            int maxNumber = number1;
-            System.out.println("max = number1 = " + number1);
+        if (number1 >= number2 & number1 >= number3 ) {
+            max = number1;
+            System.out.println("max = number1 = " + max);
         } else {
-            System.out.println("max = number2 and number3 ");
-            int maxNumber = number2;
+            min = number1;
+            System.out.println("min = number1 = " + min);
         }
-        if (number2 >= number3 || number2>=number1) {
-            int maxNumber = number2;
-            System.out.println("max = number2 = " + number2);
+        if (number2 >= number3 & number2 >= number1) {
+            max = number2;
+            System.out.println("max = number2 = " + max);
         } else {
-            System.out.println("max = number3 and number2 = ");
+            min = number2;
+            System.out.println("mix = number3 and number2 = " + number2);
         }
-        if (number3 >= number1 || number3>=number2) {
-            int maxNumber = number3;
-            System.out.println("max = number2 = " + number3);
+        if (number3 >= number1 & number3>=number2) {
+            max = number3;
+            System.out.println("max = number2 = " + max);
         } else {
-            System.out.println("max = number1 and number2");
+            min = number3;
+            System.out.println("min = number1 and number2 = " + number3);
         }
-        for (int i = 10; i >= 5; i--) {
-        System.out.print(i);
-        }   
-        System.out.println();
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+
+        for (int i=max; i>min; i=i-1) {
+            System.out.print(i);
+        }
+        System.out.println();  
     }
 }
