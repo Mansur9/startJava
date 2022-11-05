22cd92b3c4e6aa938f6e414391c7c741443b95d7
Mansur <maylur@yandex.ru>
2022-11-05 23:38:37 +0300

    My first game

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,19 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int numberLimit = 100;
+        int number = (int)(Math.random() * numberLimit);
+        int digit = 15;
+        while (true) {
+            if (number == digit) {
+                System.out.println("Вы победили!");
+                break;
+            } else if (number > digit) {
+                System.out.println(digit + " меньше того, что загадал компьютер - " + number );
+                break;
+            } else if (number < digit) {
+                System.out.println(digit + " больше того, что загадал компьютер - " + number);
+                break;
+            }
+        }
+    }
+}
