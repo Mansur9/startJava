a04c018fb74766543cdb9d7587ee99c82b18997c
Mansur <maylur@yandex.ru>
2022-11-05 22:16:08 +0300

    task Calculator

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 12234;
+        int b = 452453;
+        int result;
+        String sign = "*";
+        if (sign == "+") {
+            result = a + b;
+            System.out.println(a + " " + sign + " " + b + " = "+ result);
+        } else if  (sign == "-") {
+            result = a - b;
+            System.out.println(a + "" + sign + " " + b + " = "+ result);
+        } else if (sign == "/") {
+            result = a / b;
+            System.out.println(a + " " + sign + " " + b + " = "+ result);
+        } else if (sign == "*") {
+            result = a * b;
+            System.out.println(a + " " + sign + " " + b + " = "+ result);
+        }
+    }
+}
