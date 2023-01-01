59f5a683875100d3e8ab152a36d24acde464a088
Mansur <maylur@yandex.ru>
2023-01-01 12:06:35 +0300

    add Jaeger file

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,27 @@
+public class Jaeger {
+    private String model;
+    private String name;   
+    private double weight; 
+    private int age;  
+    private String colour;
+    private String go;
+    
+    public String setModel(String model) {
+        this.model = model;
+        return model;
+    }
+
+    public String getModel() {
+        return gender;
+    }
+
+    public String setName(String name) {
+        this.name = name;
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,15 @@
+import javax.annotation.processing.SupportedOptions;
+
+public class JaegerTest {
+    public static void main(String[] args) {
+        private String model;
+        private String name;   
+        private double weight;   
+        private String colour;
+        private String go;
+        
+        public String setModel(String model) {
+            System.out.println("модель" + model);
+        }
+    }
+}
