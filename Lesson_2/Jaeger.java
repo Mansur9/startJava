66b3b9a75a645dca4a3b2571f0e4241ffac1ab2b
Mansur <maylur@yandex.ru>
2023-01-07 06:47:33 +0300

    fix Jaeger

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,48 +1,71 @@
 public class Jaeger {
+
     private String model;
     private String mark;
     private float height;   
     private float weight; 
-    private int Speed;  
+    private int speed;  
     private int strength;
     private int amor;
     
-    public String setModel(String model) {
+    public void setModel(String model) {
         this.model = model;
-        return model;
     }
 
     public String getModel() {
         return model;
     }
 
-    public String setMark(String mark) {
+    public void setMark(String mark) {
         this.mark = mark;
+    }
+
+    public String getMark() {
         return mark;
     }
 
-    // public String getMark() {
-    //     return Mark;
-    // }
-
-    public float setHeight(float height) {
+    public void setHeight(float height) {
         this.height = height;
-        return height;
     }
 
     public float getHeight() {
         return height;
     }
 
-    // public float setWeight() {
-    //     this.
-    //     return weight;
-    // }
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setAmor(int amor) {
+        this.amor = amor;
+    }
+    public int getAmor() {
+        return amor;
+    }
 
     public void move() {
         System.out.println("moving");
     }
-
 
     public void drift() {
         System.out.println("drift");
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -2,17 +2,21 @@ import javax.annotation.processing.SupportedOptions;
 
 public class JaegerTest {
     public static void main(String[] args) {
+        
         Jaeger robot1 = new Jaeger();
         robot1.setModel("Striker Eureka");
+        robot1.getModel();
         robot1.setMark("Mark-5");
+        robot1.getMark();
         robot1.setHeight(76.2f);
-        // robot1.setWeight(1.850f);
-        // robot1.setSpeed(10);
-        // robot1.setStrength();
-        // robot1.setAmor();
-        Jaeger robot2 = new Jaeger();
+        robot1.getHeight();
+        robot1.setWeight(1.850f);
+        robot1.setSpeed(10);
+        robot1.setStrength(15);
+        robot1.setAmor(7);
 
+        Jaeger robot2 = new Jaeger();
         robot2.setModel("Khal");
-        // System.out.println("модель" + model_);
+        // System.out.println("модель" + robot2.setModel("Khal"));
     }
 }
