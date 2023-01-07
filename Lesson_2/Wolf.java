103d3a988666cd1cae82a39a1b15e4ab201779aa
Mansur <maylur@yandex.ru>
2023-01-07 06:54:09 +0300

    fix class wolf

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -7,58 +7,52 @@ public class Wolf {
     private String colour;
     private String go;
     
-    public String setGender(String gender) {
+    public void setGender(String gender) {
         this.gender = gender;
-        return gender;
     }
 
     public String getGender() {
         return gender;
     }
 
-    public String setName(String name) {
+    public void setName(String name) {
         this.name = name;
-        return name;
     }
 
     public String getName() {
         return name;
     }
 
-    public double setWeight(double weight) {
+    public void setWeight(double weight) {
         this.weight = weight;
-        return weight;
     }
 
     public double getWeight() {
         return weight;
     }
 
-    public int setAge(int age) {
+    public void setAge(int age) {
         if (age > 8) {
             System.out.println("Некорректный возраст");
         } else {
             this.age = age;
             }
-        return age;
         }
 
     public int getAge() {
         return age;
     }
 
-    public String setColour(String colour) {
+    public void setColour(String colour) {
         this.colour = colour;
-        return colour;
     }
 
     public String getColour() {
         return colour;
     }
 
-    public String setGo(String go) {
+    public void setGo(String go) {
         this.go = go;
-        return go;
     }
      public String getGo() {
         return go;
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,27 +2,27 @@ public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
         
-        String gender = wolf.setGender("man");
+        wolf.setGender("man");
         String genderGet = wolf.getGender();
         System.out.println("genderGet = " + genderGet);
         
-        String name = wolf.setName("Akela");
+        wolf.setName("Akela");
         String nameGet = wolf.getName();
         System.out.println("nameGet = " + nameGet);
 
-        double weight = wolf.setWeight(55);
+        wolf.setWeight(55);
         double weightget = wolf.getWeight();
         System.out.println("weightget = " + weightget);
         
-        int age = wolf.setAge(7);
+        wolf.setAge(7);
         int ageGet = wolf.getAge();
         System.out.println("ageGet = " + ageGet);
         
-        String colour = wolf.setColour("black");
+        wolf.setColour("black");
         String colourGet = wolf.getColour();
         System.out.println("colourGet = " + colourGet);
 
-        String go = wolf.setGo("He goes to park");
+        wolf.setGo("He goes to park");
         String goGet = wolf.getGo();
         System.out.println("goGet = " + goGet);
     }
