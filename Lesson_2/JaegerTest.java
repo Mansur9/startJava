86935b11400dffabb0e638a031e5824464ed9b21
Mansur9 <maylur@yandex.ru>
2023-01-14 08:48:36 +0800

    test

--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -18,11 +18,12 @@ public class JaegerTest {
 
         Jaeger robot2 = new Jaeger("Khal Drog","Mark-2",77,2,11,16,7);
         System.out.println("Robot2 is model - " + robot2.getModel());
-        System.out.println("Robot1 has:");
+        System.out.println("Robot1 has method:");
         robot1.move();
         robot1.attack();
-        System.out.println("Robot2 has:");
+        System.out.println("Robot2 has method :");
         robot2.drift();
         robot2.jump();
+        
     }
 }
