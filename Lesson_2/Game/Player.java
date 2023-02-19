7118a2838aac3067b41c666d3b98a503a1906df8
Мансур Сейитмедов <mansur@Noutbuk-Mansur.local>
2023-02-20 01:47:22 +0300

    fix lost task

---
+++ Lesson_2/Game/Player.java
@@ -0,0 +1,25 @@
+public class Player {
+    private String name;
+    private int number;
+    
+    Player (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }   
+    
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean setNumber(int number) {
+        this.number = number;
+        if (number < 0 || number > 100) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+ }
